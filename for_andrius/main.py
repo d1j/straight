12841712dc3a055d93c276b0507e4e935fd27759
Deividas Januska <dj9909@gmail.com>
2021-11-03 23:05:42 +0200

    fixed stuff

--- for_andrius/main.py
+++ for_andrius/main.py
@@ -1,22 +1,32 @@
 import sys, hashlib
 from bitcoin.rpc import RawProxy
-p=RawProxy() # This reverses and then swaps every other char
+
+proxy_connection=RawProxy() # This reverses and then swaps every other char
 
 def swap_order(param):
-    ba = bytearray.fromhex(param)
-    ba.reverse()
-    return(''.join(format(x, '02x') for x in ba))
+    byte_array = bytearray.fromhex(param)
+    byte_array.reverse()
+    return(''.join(format(x, '02x') for x in byte_array))
  # Get the block hash of block with height 277316
-blockhash = p.getblockhash(int(sys.argv[1]))
-  # Retrieve the block by its hash
-block = p.getblock(blockhash)
-   # Element tx contains the list of all transaction IDs in the block
-header = (swap_order(block['versionHex']) + swap_order(block['previousblockhash'])
-+ swap_order(block['merkleroot']) + swap_order('{:00x}'.format(block['time'])) + swap_order(block['bits']) +  swap_order('{:00x}'.format(block['nonce'])) )
 
-header_bin = header.decode('hex')
-hash = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
-hashenc = hash[::-1].encode('hex_codec')
-print(hashenc)
-if hashenc==block['hash']:
-    print('hashas teisingas')
+def main():
+   blockhash = proxy_connection.getblockhash(int(sys.argv[1]))
+   # Retrieve the block by its hash
+   block = proxy_connection.getblock(blockhash)
+      # Element tx contains the list of all transaction IDs in the block
+   header = (swap_order(block['versionHex']) \
+      + swap_order(block['previousblockhash']) \
+      + swap_order(block['merkleroot']) \
+      + swap_order('{:00x}'.format(block['time'])) \
+      + swap_order(block['bits']) \
+      +  swap_order('{:00x}'.format(block['nonce'])))
+
+   header_bin = header.decode('hex')
+   hash = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
+   hashenc = hash[::-1].encode('hex_codec')
+   print(hashenc)
+   if hashenc==block['hash']:
+      print('hashas teisingas')
+
+if __name__ == "__main__":
+   main()
